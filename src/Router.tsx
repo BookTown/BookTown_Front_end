5a878274a3f46e9c82409587b19947bdb0e6017b
HeyJunN <105594922+HeyJunN@users.noreply.github.com>
2025-03-28 23:11:01 +0900

    chore: 기본  파일 구조 설정, axios 설정, router 설정 #2

---
+++ src/Router.tsx
@@ -0,0 +1,31 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "./App";
+import Login from "./screens/auth/Login";
+import Register from "./screens/auth/Register";
+import Home from "./screens/home/Home";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App/>,
+        children: [
+            {
+                path: "",
+                element: <Login />,
+            },
+            {
+                path: "/register",
+                element: <Register />,
+            },
+            {
+                path: "/home",
+                element: <Home />,
+            },
+            {
+
+            }
+        ]
+    },
+]);
+
+export default router;
