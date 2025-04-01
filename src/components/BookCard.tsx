8eae62f5e0790e2a3bb61f4ac2dcdc0f57a464e3
Taemin Jo <taeeee159@gmail.com>
2025-04-01 19:46:36 +0900

    feat: 모달 컴포넌트화 및 스크롤 디자인 변경

--- src/components/BookCard.tsx
+++ src/components/BookCard.tsx
@@ -8,13 +8,13 @@ type BookCardProps = {
 const BookCard = ({ title, author, imageUrl, onClick }: BookCardProps) => {
   return (
     <div 
-      className="w-40 flex-shrink-0 rounded-xl overflow-hidden bg-white shadow-md"
+      className="w-40 h- flex-shrink-0 rounded-xl overflow-hidden bg-white shadow-md hover:shadow-lg transition hover:scale-[1.02]"
       onClick={onClick}
     >  
       <img src={imageUrl} alt={title} className="w-11/12 mt-2 rounded-xl mx-auto h-28 object-cover pb-1" />
       <div className="p-1">
         <p className="text-base">{title}</p>
-        <p className="text-sm text-gray-500">{author}</p>
+        <p className="text-sm text-[#A39C9C]">{author}</p>
       </div>
     </div>
   );
