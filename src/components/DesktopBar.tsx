082292dc7112fc6ea604be976bca5c796dc07ab6
Taemin Jo <taeeee159@gmail.com>
2025-04-05 15:33:48 +0900

    style: 사이드바 효과 추가

--- src/components/DesktopBar.tsx
+++ src/components/DesktopBar.tsx
@@ -15,13 +15,13 @@ const DesktopBar = () => {
 
   return (
     <>
-      <div className="fixed top-0 left-0 right-0 z-50 max-w-[1440px] mx-auto bg-[#FFFAF0] px-4">
+      <div className="fixed top-0 left-0 right-0 z-40 max-w-[1440px] mx-auto bg-[#FFFAF0] px-4">
         <div className="mx-auto flex justify-between items-center px-4">
           {/* 왼쪽: 햄버거 메뉴 + 로고 */}
           <div className="flex items-center gap-2">
             <button 
               onClick={() => setIsSidebarOpen(true)}
-              className="p-2 hover:bg-gray-100 rounded-full"
+              className="p-2 hover:opacity-80 rounded-full"
             >
               <Menu size={24} />
             </button>
@@ -36,52 +36,53 @@ const DesktopBar = () => {
           </div>
           
           {/* 오른쪽: 검색 버튼 */}
-          <Link to="/search" className="p-2 hover:bg-gray-100 rounded-full">
+          <Link to="/search" className="p-2 hover:opacity-80 rounded-full">
             <Search size={24} />
           </Link>
         </div>
       </div>
 
-      {/* 사이드바 */}
+      {/* 사이드바 오버레이 */}
       {isSidebarOpen && (
-        <>
-          {/* 오버레이 */}
-          <div 
-            className="fixed inset-0 bg-black/50 z-50"
-            onClick={() => setIsSidebarOpen(false)}
-          />
-          
-          {/* 사이드바 내용 */}
-          <div className="fixed top-0 left-0 bottom-0 w-64 bg-white z-50 p-4">
-            <div className="flex justify-end mb-4">
-              <button 
-                onClick={() => setIsSidebarOpen(false)}
-                className="p-2 hover:bg-gray-100 rounded-full"
-              >
-                <X size={24} />
-              </button>
-            </div>
-            <div className="flex flex-col gap-4">
-              {menus.map((menu) => {
-                const isActive = location.pathname === menu.path;
-                return (
-                  <Link
-                    to={menu.path}
-                    key={menu.name}
-                    className={`flex items-center gap-3 p-2 rounded-lg hover:bg-gray-100 ${
-                      isActive ? 'text-black' : 'text-gray-400'
-                    }`}
-                    onClick={() => setIsSidebarOpen(false)}
-                  >
-                    {menu.icon}
-                    <span className="text-base">{menu.name}</span>
-                  </Link>
-                );
-              })}
-            </div>
-          </div>
-        </>
+        <div 
+          className="fixed inset-0 bg-black/50 z-40 transition-all duration-500 ease-in-out"
+          onClick={() => setIsSidebarOpen(false)}
+        />
       )}
+          
+      {/* 사이드바 내용 */}
+      <div 
+        className={`fixed top-0 bottom-0 left-0 w-56 bg-white z-50 p-4 shadow-lg transition-transform duration-500 ease-in-out ${
+          isSidebarOpen ? 'translate-x-0' : '-translate-x-full'
+        }`}
+      >
+        <div className="flex justify-end mb-2">
+          <button 
+            onClick={() => setIsSidebarOpen(false)}
+            className="p-2 hover:opacity-80 rounded-full"
+          >
+            <X size={24} />
+          </button>
+        </div>
+        <div className="flex flex-col gap-3">
+          {menus.map((menu) => {
+            const isActive = location.pathname === menu.path;
+            return (
+              <Link
+                to={menu.path}
+                key={menu.name}
+                className={`flex items-center gap-3 p-2 rounded-lg hover:bg-gray-100 ${
+                  isActive ? 'text-black' : 'text-gray-400'
+                }`}
+                onClick={() => setIsSidebarOpen(false)}
+              >
+                {menu.icon}
+                <span className="text-base">{menu.name}</span>
+              </Link>
+            );
+          })}
+        </div>
+      </div>
     </>
   );
 };
