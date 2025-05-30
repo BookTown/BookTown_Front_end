ce6dc2053edb5d79779515a6427446a700e0dd75
HeyJunN <classjwnstj8032@naver.com>
2025-05-30 21:30:32 +0900

    fix: 신청하기 섹션 별 개별 패딩 적용 수정 #12

--- src/screens/setting/RegisterMain.tsx
+++ src/screens/setting/RegisterMain.tsx
@@ -14,14 +14,14 @@ const RegisterMain: React.FC = () => {
 
   return (
     <div className="min-h-screen bg-[#FFFAF0] text-black px-4 pt-14 pb-24 md:pt-12 md:px-6">
-      <div className="flex justify-between items-center">
+      <div className="flex justify-between items-center" style={{ paddingTop: '100px' }}>
           <div className="pt-4">
           <h1 className="text-3xl">원하는 고전 신청하기</h1>
           <p className="text-xl text-[#A39C9C] pb-2">보고싶은 책을 신청할 수 있어요</p>
         </div>
       </div>
       
-      <div style={{ paddingTop: '100px' }}>
+      <div className="pt-0 md:pt-[100px]">
         {!isInputMode && (
           <div className='flex justify-between pt-4 pb-2 px-1 max-w-[37.5rem] mx-auto'>
             <div className="text-2xl">신청 리스트</div>
@@ -33,7 +33,7 @@ const RegisterMain: React.FC = () => {
             </button>
           </div>
         )}
-        <div>
+        <div style={{ paddingTop: isInputMode ? '100px' : '0' }}>
           {isInputMode ? (
           <RegisterInput 
             onCancel={() => setIsInputMode(false)} 
